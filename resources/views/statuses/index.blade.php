98f87c73c9e8290f4a4a17234699141fbbb8240a
Arkar Hein <arkerhein44@gmail.com>
2024-10-15 21:56:50 +0630

    admin template inserted

---
+++ resources/views/statuses/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.adminindex')
+
+@section('content')
+<!-- Start Content Area  -->
+        <section>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-10 col-md-9 ms-auto">
+
+                    </div>
+                </div>
+            </div>
+        </section>
+<!-- End Content Area  -->
+@endsection
+       
+@section('styles')
+@endsection
+
+@section('scripts')
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,14 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\StatusesController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardsController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboards',[DashboardsController::class,'index'])->name('home');
+
+Route::resource('/statuses',StatusesController::class);
